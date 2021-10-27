4004f56c996809cf2089ef1fdc7718814f2f0921
Iqbal Ahemd <jniqbal1@gmail.com>
2021-10-28 00:18:46 +0600

    first commit

---
+++ components/SingleProductTwo.vue
@@ -0,0 +1,111 @@
+<template>
+  <section class="single-product">
+    <v-container>
+      <v-row class="justify-center align-center">
+        <v-col cols="12" xs="12" sm="12" md="4" lg="4">
+          <div>
+            <img src="../assets/product.png" alt="" class="img-fluid">
+          </div>
+        </v-col>
+        <v-col cols="12" lg="7" md="7" sm="12" xs="12">
+          <div class="product-details">
+            <div class="d-flex align-center">
+              <h2>Shampoo</h2>
+              <v-btn
+                color="white"
+                class="ma-2 white--dark theme-btn-sm">
+                paid
+              </v-btn>
+              <v-btn
+                color="white"
+                class="ma-2 white--dark theme-btn-sm">
+                delivered
+              </v-btn>
+            </div>
+            <p>
+              For personalised advice, you are welcome to arrange a video consultation with a trained consultant; or to
+              use our live assistance for guidance while browsing online.
+            </p>
+            <div class="product-btn">
+              <v-btn
+                color="white"
+                class="ma-2 white--dark theme-btn">
+
+                View order details
+                <v-icon right class="btn-icon-style">
+                  mdi-arrow-right
+                </v-icon>
+
+              </v-btn>
+
+              <v-btn
+                color="white"
+                class="ma-2 white--dark theme-btn"
+              >
+                View order history
+                <v-icon right class="btn-icon-style">
+                  mdi-book-open-outline
+                </v-icon>
+              </v-btn>
+            </div>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </section>
+</template>
+
+<script>
+
+
+export default {
+  name: "SingleProductTwo",
+
+}
+</script>
+
+<style scoped>
+
+
+.theme-btn {
+  padding: 25px 20px !important;
+  border: 1px solid #d0d0d0 !important;
+  box-shadow: none !important;
+  font-size: 12px !important;
+  background-color: #fff !important;
+  border-radius: 0px !important;
+}
+
+.theme-btn-sm {
+  padding: 10px 10px !important;
+  border: 1px solid #d0d0d0 !important;
+  box-shadow: none !important;
+  font-size: 8px !important;
+  background-color: #fff !important;
+  border-radius: 0px !important;
+}
+
+.product-details h2 {
+  font-weight: 300;
+  font-size: 25px;
+  padding-right: 20px;
+  margin-left: 6px;
+
+}
+
+.product-details p {
+  font-weight: 300;
+  font-size: 16px;
+  line-height: 30px;
+  margin-top: 15px;
+  margin-left: 6px;
+}
+
+.single-product {
+  background-color: #fafafa;
+}
+
+.slider {
+  width: 100%;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <div class="banner-section-bg">
+      <v-container>
+        <v-row>
+          <v-col cols="12" sm="8" md="6">
+            <div class="banner-content">
+              <h1>Welcome Remi</h1>
+              <v-btn
+                color="white"
+                class="ma-2 white--dark theme-btn">
+                Change personal details
+                <v-icon right class="btn-icon-style">
+                  mdi-cloud-upload
+                </v-icon>
+              </v-btn>
+            </div>
+          </v-col>
+
+        </v-row>
+      </v-container>
+      <v-row>
+        <v-col cols="12">
+          <img src="../assets/banner.png" alt="" class="banner-img">
+        </v-col>
+      </v-row>
+    </div>
+
+    <SingleProduct/>
+    <hr>
+    <SingleProductTwo/>
+  </div>
+</template>
+<style scoped>
+.theme-btn {
+  padding: 25px 20px !important;
+  border: 1px solid #d0d0d0 !important;
+  box-shadow: none !important;
+  font-size: 12px !important;
+  background-color: #fff !important;
+  border-radius: 0px !important;
+}
+
+.btn-icon-style {
+  margin-left: 20px !important;
+}
+
+.banner-content {
+  padding-top: 100px;
+
+}
+
+.banner-content h1 {
+  font-weight: 300;
+  margin-left: 6px;
+  margin-bottom: 10px;
+}
+
+.banner-section-bg {
+  background-color: #fafafa;
+  /*background-color: #d0d0d0;*/
+  /*display: flex;*/
+  /*align-items: center;*/
+  /*background-image: url(./assets/banner.png);*/
+  /*object-position: bottom;*/
+  /*object-fit: scale-down;*/
+}
+
+.banner-section-bg img {
+  height: 150px;
+  padding-top: 20px;
+  margin-bottom: 10px;
+  width: 100%;
+}
+</style>
+<script>
+import SingleProduct from "../components/SingleProduct";
+import SingleProductTwo from "../components/SingleProductTwo";
+
+export default {
+  components: {SingleProduct, SingleProductTwo}
+}
+</script>
